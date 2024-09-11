a48c9080fa01058178cade3390c5d260808e543a
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2024-09-11 21:41:14 +0530

    todo add and delete route

--- index.js
+++ index.js
@@ -75,6 +75,47 @@ app.get("/todos",(req,res)=>{
     }
 })
 
+app.post("/todos",(req,res)=>{
+    try{
+        const token = req.headers.token;
+        const todo = req.body.todo
+        const decodedUsername = jwt.verify(token,JWT_SECRET)
+
+        let foundUser = null;
+
+        foundUser = users.find(user => user.username === decodedUsername.username)
+
+        if(foundUser){
+            foundUser = {...foundUser,todos:[...foundUser.todos,todo]}
+        }
+
+        res.json({message:`${todo} added to User ${foundUser.username}`})
+    }catch(err){
+        res.json({message:"Could not add todo", error:err})
+    }
+})
+
+app.delete("/todos",(req,res)=>{
+    try{
+        const token = req.headers.token;
+        const todoId = req.body.id;
+        const decodedUsername = jwt.verify(token,JWT_SECRET)
+
+        let foundUser = null;
+
+        foundUser = users.find(user => user.username === decodedUsername.username)
+
+        if(foundUser){
+            foundUser.todos = foundUser.todos.filter(todo => todo.id !== todoId)
+        }
+
+        res.json({message:`todo with${id} removed`})
+      
+
+    }catch(err){
+        res.json({message:"Codunt delete todo"})
+    }
+})
 app.listen(PORT , ()=>{
     console.log("App is running")
 })
